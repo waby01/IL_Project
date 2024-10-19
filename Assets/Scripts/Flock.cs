2ee7488c353638f40ec2f3f029850cd58cdf0358
waby01 <141507493+waby01@users.noreply.github.com>
2024-10-19 16:12:15 +0700

    Project Files

--- Assets/Scripts/Flock.cs
+++ Assets/Scripts/Flock.cs
@@ -8,15 +8,16 @@ public class Flock : MonoBehaviour
     [Header("Spawn Setup")]
     [SerializeField] private FlockUnit flockUnitPrefab;
     [SerializeField] private int flockSize;
-     [SerializeField] private Vector3 spawnBounds;
+    [SerializeField] private Vector3 spawnBounds;
 
-      [Header("Speed Setup")]
-      [Range(0, 10)]
-      [SerializeField] private float _minSpeed;
-       public float minSpeed { get { return _minSpeed; } }
-      [Range(0, 10)]
-       [SerializeField] private float _maxSpeed;
-       public float maxSpeed { get { return _maxSpeed; } }
+    [Header("Speed Setup")]
+    [Range(0, 10)]
+    [SerializeField] private float _minSpeed;
+    public float minSpeed { get { return _minSpeed; } }
+    [Range(0, 10)]
+    [SerializeField] private float _maxSpeed;
+    public float maxSpeed { get { return _maxSpeed; } }
+
 
     [Header("Detection Distances")]
 
@@ -32,6 +33,15 @@ public class Flock : MonoBehaviour
     [SerializeField] private float _aligementDistance;
     public float aligementDistance { get { return _aligementDistance; } }
 
+    [Range(0, 10)]
+    [SerializeField] private float _obstacleDistance;
+    public float obstacleDistance { get { return _obstacleDistance; } }
+
+    [Range(0, 100)]
+    [SerializeField] private float _boundsDistance;
+    public float boundsDistance { get { return _boundsDistance; } }
+
+
     [Header("Behaviour Weights")]
 
     [Range(0, 10)]
@@ -45,36 +55,42 @@ public class Flock : MonoBehaviour
     [Range(0, 10)]
     [SerializeField] private float _aligementWeight;
     public float aligementWeight { get { return _aligementWeight; } }
-  
-   
 
-     public FlockUnit[] allUnits {get; set;}
+    [Range(0, 10)]
+    [SerializeField] private float _boundsWeight;
+    public float boundsWeight { get { return _boundsWeight; } }
 
-     private void Start() {
-        Generateunits();
+    [Range(0, 100)]
+    [SerializeField] private float _obstacleWeight;
+    public float obstacleWeight { get { return _obstacleWeight; } }
 
-        
-     }
-     private void Update()
+    public FlockUnit[] allUnits { get; set; }
+
+    private void Start()
+    {
+        GenerateUnits();
+    }
+
+    private void Update()
     {
         for (int i = 0; i < allUnits.Length; i++)
         {
             allUnits[i].MoveUnit();
         }
     }
-    private void Generateunits()
+
+    private void GenerateUnits()
     {
-        allUnits = new  FlockUnit[flockSize];
+        allUnits = new FlockUnit[flockSize];
         for (int i = 0; i < flockSize; i++)
         {
             var randomVector = UnityEngine.Random.insideUnitSphere;
             randomVector = new Vector3(randomVector.x * spawnBounds.x, randomVector.y * spawnBounds.y, randomVector.z * spawnBounds.z);
             var spawnPosition = transform.position + randomVector;
-            var rotation = Quaternion.Euler(0, UnityEngine.Random.Range(0,360),0);
+            var rotation = Quaternion.Euler(0, UnityEngine.Random.Range(0, 360), 0);
             allUnits[i] = Instantiate(flockUnitPrefab, spawnPosition, rotation);
             allUnits[i].AssignFlock(this);
             allUnits[i].InitializeSpeed(UnityEngine.Random.Range(minSpeed, maxSpeed));
         }
-
     }
 }

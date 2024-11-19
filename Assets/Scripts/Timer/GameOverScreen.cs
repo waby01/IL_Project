f3007fd73aa3d38a22cc01f71dadb3064fed1667
KhalidMlnaaa <khaaliddd055@gmail.com>
2024-11-20 01:10:35 +0800

    Revert "uiScript"
    This reverts commit a769e7d7722a9a821f8fef332f31409013df821c.

--- Assets/Scripts/Timer/GameOverScreen.cs
+++ Assets/Scripts/Timer/GameOverScreen.cs
@@ -1,35 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using TMPro;
 
 public class GameOverScreen : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI gameOverText;
 
-    public void setup(string message)
+    public void setup()
     {
-        Debug.Log("Game Over setup called");
+        gameObject.SetActive(true);
+        
 
-        gameObject.SetActive(true); // Aktifkan UI Game Over
-
-        if (gameOverText != null)
-        {
-            gameOverText.text = message;  // Set pesan Game Over
-            Debug.Log("Game Over Text Set: " + message);
-        }
-        else
-        {
-            Debug.LogError("Game Over Text not assigned!");
-        }
-
-        Cursor.visible = true; // Tampilkan kursor
-        Cursor.lockState = CursorLockMode.None; // Lepaskan kursor
     }
-
 
     public void restartButton()
     {
-        Time.timeScale = 1; // Reset waktu saat restart
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Reload scene saat ini
+        
+        SceneManager.LoadScene(0); // Reload the current scene
+        
     }
+
+
 }
--- Assets/Scripts/Timer/Timer.cs
+++ Assets/Scripts/Timer/Timer.cs
@@ -7,39 +7,46 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
-    [SerializeField] float remainingTime;
-    public GameOverScreen gameOverScreen;
 
-    // Flag untuk memastikan game over hanya dipanggil sekali
-    private bool gameOverCalled = false;
+    [SerializeField] float remainingTime;
+
+    public GameOverScreen gameOverScreen;
 
     // Update is called once per frame
     void Update()
     {
-        if (remainingTime > 0)
+        if (remainingTime > 0 )
         {
             remainingTime -= Time.deltaTime;
             if (remainingTime <= 4)
             {
                 timerText.color = Color.red;
-            }
-        }
-        else if (remainingTime <= 0 && !gameOverCalled)
-        {
-            remainingTime = 0;
-            gameOverCalled = true; // Tandai bahwa game over sudah dipanggil
-            gameOver(); // Panggil game over hanya sekali
-            Time.timeScale = 0; // Hentikan waktu
-        }
 
-        // Menampilkan timer
+            }
+
+        }
+         else if (remainingTime <= 0) {
+            remainingTime = 0;
+            Time.timeScale = 0;
+            gameOver();
+          
+
+
+        }
+       
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText.text = string.Format ("{0:00}:{1:00}", minutes, seconds);
+
     }
 
     public void gameOver()
     {
-        gameOverScreen.setup("Time is out!");
+     
+        gameOverScreen.setup();
+        
+
+
+
     }
 }

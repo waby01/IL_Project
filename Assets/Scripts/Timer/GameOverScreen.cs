a769e7d7722a9a821f8fef332f31409013df821c
KhalidMlnaaa <khaaliddd055@gmail.com>
2024-11-19 22:20:09 +0800

    uiScript
    v1

--- Assets/Scripts/Timer/GameOverScreen.cs
+++ Assets/Scripts/Timer/GameOverScreen.cs
@@ -1,25 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameOverScreen : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI gameOverText;
 
-    public void setup()
+    public void setup(string message)
     {
-        gameObject.SetActive(true);
-        
+        Debug.Log("Game Over setup called");
 
+        gameObject.SetActive(true); // Aktifkan UI Game Over
+
+        if (gameOverText != null)
+        {
+            gameOverText.text = message;  // Set pesan Game Over
+            Debug.Log("Game Over Text Set: " + message);
+        }
+        else
+        {
+            Debug.LogError("Game Over Text not assigned!");
+        }
+
+        Cursor.visible = true; // Tampilkan kursor
+        Cursor.lockState = CursorLockMode.None; // Lepaskan kursor
     }
+
 
     public void restartButton()
     {
-        
-        SceneManager.LoadScene(0); // Reload the current scene
-        
+        Time.timeScale = 1; // Reset waktu saat restart
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Reload scene saat ini
     }
-
-
 }
